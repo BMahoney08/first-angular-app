1fdc57a774c728e0ac57db8268a6df74f6a991ab
Brendan Mahoney <bmahoney@actionet.com>
2020-01-14 09:36:17 -0500

    Merging with home local

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
 
-  constructor() { }
+
+  constructor() { 
+    setTimeout(() => {
+     this.allowNewServer = true },2000
+    )}
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
